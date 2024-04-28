589259a2399fef6f7c514fe68892243b038c95a7
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-28 07:41:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if(str.match(mailformat)){
+		return true;
+	}
+	else return false;
 }
 
 // Do not change the code below.
